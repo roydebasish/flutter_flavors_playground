5efcc9982bd9d60179681dcae3af4a6d3a2955d9
roydebasish <roydebasish0010@gmail.com>
2024-02-10 00:48:30 +0600

    project added to github

---
+++ lib/main_dev.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import 'AppConfig.dart';
+import 'home_page.dart';
+
+void main() async {
+  AppConfig.create(
+    appName: "Dev Flavor Example",
+    baseUrl: "https://dev.dwirandyh.com",
+    primaryColor: Colors.blue,
+    flavor: Flavor.dev,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo Production',
+      theme: ThemeData(
+        primarySwatch: AppConfig.shared.primaryColor,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
